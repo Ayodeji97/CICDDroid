49ee59475c5f8ba2ef7a9648201015444912323e
Ayodeji97 <danielayodeji97@gmail.com>
2022-08-05 07:25:23 +0100

    Initial commit message

---
+++ app/src/main/java/com/financials/retirementcalculator/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.financials.retirementcalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        AppCenter.start(application, "130e0dbe-f201-484b-b192-a07f3cc6fb4b", Analytics::class.java, Crashes::class.java)
+
+    }
+}
